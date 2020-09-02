477ff963736a10af03e8196bb9cb3119f41c0cbe
Mari Wronka <marielle.wronka-ext@viavarejo.com.br>
2020-09-01 22:39:24 -0300

    exercicio01

---
+++ exercicio01/README.md
@@ -0,0 +1,17 @@
+# Exercício 1
+
+Esse exercício tem como intuíto exercitar questão de posicionamento de Widgets.
+
+## Instruções
+- Deverá ser criado uma tela, semelhante a figura a seguir:
+- ![alt text](tela-exercicio.png)
+
+### Porcentagem das linhas
+- Primeira linha usa 20% da altura da tela
+- Segunda linha usa 30% da altura da tela
+- Terceira linha usa 40% da altura da tela
+
+### Cores das linhas
+- Colors.purple[100], Colors.purple[200], Colors.purple[300]
+- Colors.red[100], Colors.red[200]
+- Colors.green[100], Colors.green[200], Colors.green[300], Colors.green[400]
---
+++ exercicio01/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Exercício 1'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 20,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.purple[100],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.purple[200],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.purple[300],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 30,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.red[100],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(color: Colors.red[200]),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 50,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.green[100],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(color: Colors.green[200]),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.green[300],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.green[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
